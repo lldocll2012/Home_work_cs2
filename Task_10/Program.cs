619866c196fbe7227c61ba1494b54389402d1aaa
Egor <lldocll2012@outlook.com>
2023-07-18 13:57:55 +0300

    end task

---
+++ Task_10/Program.cs
@@ -0,0 +1,18 @@
+﻿int Input()
+{
+    Console.WriteLine("Введите трехзначное число: ");
+    int number = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+    return number;
+}
+
+
+int a = Input();
+while (a < 100 || a >= 1000) 
+{
+    Console.WriteLine("Это не трехзначное число");
+    a = Input();
+}
+int result = a / 10 % 10; 
+Console.WriteLine($"Вторая цифра числа: {result}");
+
+        
